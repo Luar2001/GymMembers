5efecaf63a3c747ae21a1b7898cb72a2fb78b871
BuildTools <unconfigured@null.spigotmc.org>
2020-10-11 16:53:14 +0200

    Split3Way Can now Divide up the ArrayList of Customers into a multidimensional array that represents 1 customer each

--- src/Split3Way.java
+++ src/Split3Way.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by Lukas Aronsson
@@ -11,18 +12,23 @@ public class Split3Way {
 
     private int CustomersSize;
 
-    protected String[][] customer = new String[this.CustomersSize][4];
+    protected String[][] customer = new String[100][4];
 
     public Split3Way(ArrayList<String> customers){
+
+        //sets the size of the arraylist from ReadFile
         setCustomersSize(customers);
 
+        //sorts the Arraylist Based on index into the multidimensional array
         SplitListContainingAPerson(customers);
 
+        //prints the entire array
+        PrintCustomer();
 
     }
 
     public int getCustomersSize() {
-        return CustomersSize;
+        return CustomersSize-1;
     }
 
     public void setCustomersSize(ArrayList<String> customers) {
@@ -31,20 +37,30 @@ public class Split3Way {
 
     public void SplitListContainingAPerson(ArrayList<String> customers){
 
-        //i is for index and g is for multidimensional index
-        for(int i=0, g=0; i < CustomersSize; i++,  g++){
+        //i is for index in array h is for index in arraylist and g is for multidimensional index
+        for(int i=0, g=0, h=0; h < customers.size(); g++, h++){
 
             //if g exceeds 3 it will turn to 1 (its only for spaces in each of the multi arrays
             if(g==4){
                 g = 0;
+                i++;
             }
 
             //takes the string on index i and adds it in index i g in the multidimensional array
-            customer[i][g]=customers.get(i);
+            customer[i][g]=customers.get(h);
 
 
         }
 
+    }
+
+    /**
+     * Prints the entire Multidimensional array (FOR TESTING)
+     */
+    public void PrintCustomer(){
+        System.out.println("ARRAY PRINT");
+        for (String[] s : customer)
+            System.out.println(Arrays.toString(s));
     }
 
     public void CheckIdNumber(){
